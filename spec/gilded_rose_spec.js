cbb1069ec4d77d19b4b6a86e252063d709151615
Tomas Doherty <tomasdoh1@gmail.com>
2019-02-15 18:10:50 +0000

    Test for checking quality can't be a negative value

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,12 +1,18 @@
 const { Shop, Item } = require('../src/gilded_rose.js');
-describe("Gilded Rose", () => {
+describe("Gilded Rose -", () => {
 
-  describe("No edge cases", () => {
-    it("quality and sell date should lower by one", () => {
+  describe("Normal items:", () => {
+    it("quality and sell-in date should lower by one", () => {
       const gildedRose = new Shop([ new Item("cheese", 10, 10) ]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(9);
       expect(items[0].quality).toEqual(9);
     });
+
+    it("the quality of an item can't be negative", () => {
+      const gildedRose = new Shop([ new Item("cheese", 0, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).not.toEqual(-1);
+    })
   });
 });
