3880d701f4c2b1800b0434786679bff34fd2f134
Tomas Doherty <tomasdoh1@gmail.com>
2019-02-15 17:03:15 +0000

    Passing test for normal items
    Checks that sellIn and quality decrease by one each time updateQuality 
    is called

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,12 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const { Shop, Item } = require('../src/gilded_rose.js');
+describe("Gilded Rose", () => {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  describe("No edge cases", () => {
+    it("quality and sell date should lower by one", () => {
+      const gildedRose = new Shop([ new Item("cheese", 10, 10) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(9);
+    });
   });
-
 });
